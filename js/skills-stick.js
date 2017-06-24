325faed4b2659238a08949a0df566a77d6388668
Juan Carlos <juancagb.17@gmail.com>
2017-06-24 13:56:39 -0500

    Responsive slick carousel

--- js/skills-stick.js
+++ js/skills-stick.js
@@ -7,7 +7,30 @@ jQuery(function () {
 		autoplay: true,
 		autoplaySpeed: 2500,
 		arrows: false,
-		inifinite: true
+		inifinite: true,
+		responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 3,
+	        slidesToScroll: 1
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 1
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	  ]
 	});
 	$('img.skill').css('opacity', 1);
 });
